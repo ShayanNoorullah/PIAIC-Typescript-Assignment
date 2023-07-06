59b2a4a93f32edc07b30311fc1b3d0bca66c5bdd
ShayanNoorullah <89250613+ShayanNoorullah@users.noreply.github.com>
2023-07-06 19:33:12 +0500

    Add files via upload

---
+++ Excersice 2/PersonalMessage.js
@@ -0,0 +1,2 @@
+var PersonName = "Shayan";
+console.log("Hello ".concat(PersonName, ", would you like to learn some Typescript today?"));
---
+++ Excersice 2/PersonalMessage.ts
@@ -0,0 +1,2 @@
+let PersonName: string = "Shayan";
+console.log(`Hello ${PersonName}, would you like to learn some Typescript today?`);
